ff8b38c5e912fb93cb421a4785196915bbabad4f
Renata Cummins <renatacummins@gmail.com>
2019-04-24 14:34:47 -0400

    Modify Display#render to show cursor position in green

--- lib/display.rb
+++ lib/display.rb
@@ -10,18 +10,32 @@ class Display
     def render
         (0..7).reverse_each do |row|
             (0..7).each do |column|
-                piece = @board[[row, column]]
+                pos = [row, column]
+                piece = @board[pos]
                 piece_color = ((piece.nil?) ? :white : piece.color)
                 display_code = ((piece.nil?) ? '  ' : 'X ')
 
+                background = case
+                when @cursor.cursor_pos == pos
+                    :cursor_here
+                when (row + column).odd?
+                    :dark
+                when (row + column).even?
+                    :light
+                end
+
                 case
-                when piece_color == :white && (row + column).odd?
+                when piece_color == :white && background == :cursor_here
+                    print display_code.white.on_green
+                when piece_color == :white && background == :dark
                     print display_code.white.on_magenta
-                when piece_color == :white && (row + column).even?
+                when piece_color == :white && background == :light
                     print display_code.white.on_light_magenta
-                when piece_color == :black && (row + column).odd?
+                when piece_color == :black && background == :cursor_here
+                    print display_code.black.on_green
+                when piece_color == :black && background == :dark
                     print display_code.black.on_magenta
-                when piece_color == :black && (row + column).even?
+                when piece_color == :black && background == :light
                     print display_code.black.on_light_magenta
                 end
             end
