afe81a162f68ce4335ef4fe7c3a4c1914c7b5ee6
Renata Cummins <renatacummins@gmail.com>
2019-04-24 11:57:55 -0400

    Fill in Cursor#handle_key

--- lib/cursor.rb
+++ lib/cursor.rb
@@ -76,6 +76,15 @@ class Cursor
   end
 
   def handle_key(key)
+    case key
+    when :return, :space
+      @cursor_pos
+    when :left, :right, :up, :down
+      update_pos(MOVES[key])
+      return nil
+    when :ctrl_c
+      Process.exit(0)
+    end
   end
 
   def update_pos(diff)
