13926328d8d9f9655f9b36d9f8fee991e55986f9
Renata Cummins <renatacummins@gmail.com>
2019-04-24 21:11:42 -0400

    Modify Slideable#moves to allow a piece to take an opponent's piece

--- lib/slideable.rb
+++ lib/slideable.rb
@@ -12,9 +12,17 @@ module Slideable
 
         move_dirs.each do |dir|
             new_pos = [@pos[0] + dir[0], @pos[1] + dir[1]]
+
+            # keep moving until the position is not empty or is off the board
             while @board[new_pos].nil? && @board.valid_pos?(new_pos)
                 end_positions << new_pos
                 new_pos = [new_pos[0] + dir[0], new_pos[1] + dir[1]]
+            end
+
+            # if the position is occupied by a piece of the opposite color, 
+            # add this position to the list of moves
+            if !@board[new_pos].nil? && @board[new_pos].color != @color
+                end_positions << new_pos
             end
         end
 
--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -9,7 +9,7 @@ describe 'Rook' do
     describe 'Slideable#moves' do
         it 'returns the correct array of possible end positions' do
             expected_res = [[3,2], [3,1], [3,0], [3,4], [3,5], [3,6], [3,7], 
-                [4,3], [5,3], [2,3]]
+                [4,3], [5,3], [6,3], [2,3]]
             expect(rook.moves).to eq(expected_res)
         end
     end
