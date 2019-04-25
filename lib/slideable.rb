d1c30eb0a6917e13c6be0d1999cd86485e24a414
Renata Cummins <renatacummins@gmail.com>
2019-04-24 20:58:23 -0400

    Add Slideable#moves

--- lib/slideable.rb
+++ lib/slideable.rb
@@ -7,6 +7,20 @@ module Slideable
         DIAGONAL_DIRS
     end
 
+    def moves
+        end_positions = []
+
+        move_dirs.each do |dir|
+            new_pos = [@pos[0] + dir[0], @pos[1] + dir[1]]
+            while @board[new_pos].nil? && @board.valid_pos?(new_pos)
+                end_positions << new_pos
+                new_pos = [new_pos[0] + dir[0], new_pos[1] + dir[1]]
+            end
+        end
+
+        return end_positions
+    end
+
     private 
 
     HORIZONTAL_DIRS = [[0, -1], [0, 1], [1, 0], [-1, 0]]
---
+++ spec/rook_spec.rb
@@ -0,0 +1,16 @@
+require 'RSpec'
+require 'rook'
+require 'board'
+
+describe 'Rook' do
+    let(:board) { Board.new }
+    let(:rook) { Rook.new(:black, board, [3, 3]) }
+
+    describe 'Slideable#moves' do
+        it 'returns the correct array of possible end positions' do
+            expected_res = [[3,2], [3,1], [3,0], [3,4], [3,5], [3,6], [3,7], 
+                [4,3], [5,3], [2,3]]
+            expect(rook.moves).to eq(expected_res)
+        end
+    end
+end
