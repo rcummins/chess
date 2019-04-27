c0c4070dba2600b8907dc27e3828c51ed0faea9a
Renata Cummins <renatacummins@gmail.com>
2019-04-27 12:15:44 -0400

    Add test for Piece#valid_moves when used by bishop

--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -13,4 +13,19 @@ describe 'Bishop' do
             expect(bishop.moves).to eq(expected_result)
         end
     end
+
+    describe 'Piece#valid_moves' do
+        it 'returns the correct positions when king is in check' do
+            board.move_piece([6,4], [4,4])
+            board.move_piece([1,4], [2,4])
+            board.move_piece([6,5], [5,5])
+            board.move_piece([0,3], [3,6])
+            board.move_piece([6,6], [5,6])
+            board.move_piece([3,6], [5,4])
+            expected_moves = [[6, 6], [5, 7], [6, 4], [5, 3], [4, 2], [3, 1], 
+                [2, 0]]
+            expect(board[[7,5]].moves).to eq(expected_moves)
+            expect(board[[7,5]].valid_moves).to eq([[6,4]])
+        end
+    end
 end
