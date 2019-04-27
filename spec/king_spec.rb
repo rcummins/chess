65af86165ab4a92667b5adedd3b9f7a17fce83c8
Renata Cummins <renatacummins@gmail.com>
2019-04-27 11:51:44 -0400

    Add test for Piece#valid_moves when used by king

--- spec/king_spec.rb
+++ spec/king_spec.rb
@@ -32,4 +32,17 @@ describe 'King' do
             expect(king.moves).to eq(expected_results)
         end
     end
+
+    describe 'Piece#valid_moves' do
+        it 'returns correct positions when king is in check' do
+            board.move_piece([6,5], [5,5])
+            board.move_piece([1,4], [3,4])
+            board.move_piece([6,6], [4,6])
+            board.move_piece([1,0], [2,0])
+            board.move_piece([6,4], [5,4])
+            board.move_piece([0,3], [4,7])
+            expect(board[[7,4]].moves).to eq([[6,4], [6,5]])
+            expect(board[[7,4]].valid_moves).to eq([[6,4]])
+        end
+    end
 end
