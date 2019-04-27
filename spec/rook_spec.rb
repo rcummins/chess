d6d339387cfdaa6ba9b9f4116ee5ff24e76a630d
Renata Cummins <renatacummins@gmail.com>
2019-04-27 12:23:48 -0400

    Add test for Piece#valid_moves when used by rook

--- spec/rook_spec.rb
+++ spec/rook_spec.rb
@@ -13,4 +13,21 @@ describe 'Rook' do
             expect(rook.moves).to eq(expected_result)
         end
     end
+
+    describe 'Piece#valid_moves' do
+        it 'returns the correct positions when king is in check' do
+            board.move_piece([6,4], [4,4])
+            board.move_piece([1,4], [2,4])
+            board.move_piece([6,5], [5,5])
+            board.move_piece([0,3], [3,6])
+            board.move_piece([6,6], [5,6])
+            board.move_piece([1,0], [2,0])
+            board.move_piece([6,7], [5,7])
+            board.move_piece([1,1], [2,1])
+            board.move_piece([7,7], [6,7])
+            board.move_piece([3,6], [5,4])
+            expect(board[[6,7]].moves).to eq([[6,6], [6,5], [6,4], [7,7]])
+            expect(board[[6,7]].valid_moves).to eq([[6,4]])
+        end
+    end
 end
